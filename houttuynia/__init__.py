e69e53a35da07b2c1e739da7b5303113b7813415
speedcell4 <speedcell4@gmail.com>
2018-05-24 00:42:50 +0900

    framework

--- houttuynia/__init__.py
+++ houttuynia/__init__.py
@@ -1,3 +1,87 @@
+import random
+
+import torch
+from torch import cuda
+import numpy as np
+
 from . import datasets
 from . import models
 from . import nn
+
+
+def manual_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    cuda.manual_seed_all(seed)
+
+
+class Configuration(dict):
+    def __getattr__(self, item):
+        return super(Configuration, self).__getitem__(item)
+
+    def __setattr__(self, key, value):
+        return super(Configuration, self).__setitem__(key, value)
+
+    def __delattr__(self, item):
+        return super(Configuration, self).__delitem__(item)
+
+
+config = Configuration(
+    chapter='train',
+    device=torch.device('cpu'),
+)
+
+
+def i8_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.int8)
+
+
+def i16_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.int16)
+
+
+def i32_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.int)
+
+
+def i64_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.long)
+
+
+def u8_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.uint8)
+
+
+def u16_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.uint16)
+
+
+def u32_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.uint)
+
+
+def u64_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.ulong)
+
+
+def f16_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.float16)
+
+
+def f32_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.float32)
+
+
+def f64_tensor(tensor):
+    return torch.tensor(tensor, device=config.device, dtype=torch.float64)
+
+
+def using_config(**kwargs):
+    global config
+    old_config = {**config}
+    config = {**old_config, **kwargs}
+    try:
+        yield
+    finally:
+        config = old_config
---
+++ houttuynia/schedules/schedule.py
@@ -0,0 +1,89 @@
+from datetime import datetime
+
+import torch
+from torch.utils.data import DataLoader
+from torch import nn, optim
+import logbook as log
+
+_observation = {}
+
+
+def report_scalar(name: str, value: float):
+    global _observation
+    return _observation.setdefault(name, []).append(value)
+
+
+def average_scalar(name: str) -> float:
+    obs = _observation.get(name, [])
+    return sum(obs) / max(1, len(obs))
+
+
+class Extension(object):
+    def __call__(self, schedule: 'Schedule') -> None:
+        raise NotImplementedError
+
+
+class Schedule(object):
+    accumulated_loss: torch.Tensor
+
+    def __init__(self, module: nn.Module, optimizer: optim.Optimizer):
+        super(Schedule, self).__init__()
+
+        self.module = module
+        self.optimizer = optimizer
+
+        self.before_epoch_extensions = []
+        self.after_epoch_extensions = []
+        self.before_iteration_extensions = []
+        self.after_iteration_extensions = []
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        self.epoch = 0
+        self.iteration = 0
+
+    def register_extension(self, extension: 'Extension',
+                           epoch: int = None, iteration: int = None, before: bool = False) -> None:
+        raise NotImplementedError
+
+    def before_epoch(self):
+        self.epoch += 1
+        self.epoch_start_tm = datetime.now()
+        log.notice(f'[{self.epoch} epoch] begin, current time: {self.epoch_start_tm}')
+
+        for extension in self.before_epoch_extensions:
+            extension.__call__(self)
+
+    def after_epoch(self):
+        for extension in self.after_epoch_extensions:
+            extension.__call__(self)
+        log.notice(f'[{self.epoch} epoch] done, elapse time: {datetime.now() - self.epoch_start_tm}')
+
+    def before_iteration(self):
+        self.iteration += 1
+
+        for extension in self.before_iteration_extensions:
+            extension.__call__(self)
+
+    def after_iteration(self):
+        loss = self.accumulated_loss.item()
+        log.info(f'[{self.epoch}/{self.iteration}] loss => {loss:.4f}')
+
+        for extension in self.after_iteration_extensions:
+            extension.__call__(self)
+
+        return self.optimizer.step()
+
+    def train(self, data_loader: DataLoader, num_epochs: int):
+        for _ in range(num_epochs):
+            self.before_epoch()
+            for batch in data_loader:
+                self.before_iteration()
+                self.module.train()
+                self.accumulated_loss = self.module(batch)
+                self.after_iteration()
+            self.after_epoch()
+
+    def eval(self, data_loader: DataLoader):
+        raise NotImplementedError
