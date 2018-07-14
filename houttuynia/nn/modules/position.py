e4e49551675d37308f83662b5104b367424cece7
speedcell4 <speedcell4@gmail.com>
2018-07-14 15:45:05 +0900

    update(position): split idx_pos_indexing and seq_pos_indexing

--- houttuynia/nn/modules/position.py
+++ houttuynia/nn/modules/position.py
@@ -5,17 +5,22 @@ import torch
 import houttuynia as ho
 
 
-def position_indexes(sequence: List, token=None, total_length: int = None,
-                     offset: int = None, min_value: int = None, max_value: int = None):
+def idx_pos_indexing(token_idx: int, total_length: int,
+                     offset: int, min_value: int = None, max_value: int = None):
     if offset is None:
         offset = 0
-    if total_length is None:
-        total_length = len(sequence)
-
-    token = sequence.index(token) if token is not None else 0
-
     with torch.no_grad():
-        idx = ho.long_tensor(torch.arange(0, total_length)) - token
+        idx = ho.long_tensor(torch.arange(0, total_length)) - token_idx
         if min_value is not None or max_value is not None:
             idx = torch.clamp(idx, min=min_value, max=max_value)
         return idx + offset
+
+
+def seq_pos_indexing(sequence: List, token=None, total_length: int = None,
+                     offset: int = None, min_value: int = None, max_value: int = None):
+    if total_length is None:
+        total_length = len(sequence)
+
+    token_idx = sequence.index(token) if token is not None else 0
+    return idx_pos_indexing(token_idx=token_idx, total_length=total_length,
+                            offset=offset, min_value=min_value, max_value=max_value)
--- tests/test_nn/test_modules/test_position.py
+++ tests/test_nn/test_modules/test_position.py
@@ -3,30 +3,30 @@ import unittest
 import torch
 
 import houttuynia as ho
-from nn.modules.position import position_indexes
+from nn.modules.position import seq_pos_indexing
 
 
-class TestPositionIndexes(unittest.TestCase):
+class TestSeqPosIndexing(unittest.TestCase):
     def test_token_0(self):
         tensor1 = [1, 2, 3, 4]
         tensor2 = ho.long_tensor([0, 1, 2, 3])
-        tensor1 = position_indexes(tensor1)
+        tensor1 = seq_pos_indexing(tensor1)
         self.assertTrue(torch.equal(tensor1, tensor2), msg=f'{tensor1} != {tensor2}')
 
     def test_token_1(self):
         tensor1 = [1, 2, 3, 4]
         tensor2 = ho.long_tensor([-1, 0, 1, 2])
-        tensor1 = position_indexes(tensor1, token=2)
+        tensor1 = seq_pos_indexing(tensor1, token=2)
         self.assertTrue(torch.equal(tensor1, tensor2), msg=f'{tensor1} != {tensor2}')
 
     def test_token_2(self):
         tensor1 = [1, 2, 3, 4]
         tensor2 = ho.long_tensor([3, 4, 5, 6])
-        tensor1 = position_indexes(tensor1, token=2, offset=4)
+        tensor1 = seq_pos_indexing(tensor1, token=2, offset=4)
         self.assertTrue(torch.equal(tensor1, tensor2), msg=f'{tensor1} != {tensor2}')
 
     def test_token_3(self):
         tensor1 = [1, 2, 3, 4]
         tensor2 = ho.long_tensor([3, 4, 5, 5])
-        tensor1 = position_indexes(tensor1, token=2, offset=4, min_value=-1, max_value=+1)
+        tensor1 = seq_pos_indexing(tensor1, token=2, offset=4, min_value=-1, max_value=+1)
         self.assertTrue(torch.equal(tensor1, tensor2), msg=f'{tensor1} != {tensor2}')
