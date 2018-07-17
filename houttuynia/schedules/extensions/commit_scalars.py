6c2111f52f769b313fc2fece32afa54a67cfece6
speedcell4 <speedcell4@gmail.com>
2018-07-17 14:48:41 +0900

    fix

--- houttuynia/schedules/extensions/commit_scalars.py
+++ houttuynia/schedules/extensions/commit_scalars.py
@@ -1,10 +1,8 @@
 from houttuynia.schedules import Extension, Schedule
-from houttuynia import long_tensor
 
 __all__ = [
     'CommitScalarBySum',
     'CommitScalarByMean',
-    'CommitPRCurve',
 ]
 
 
@@ -16,7 +14,7 @@ class CommitScalarBySum(Extension):
 
     def __call__(self, schedule: 'Schedule') -> None:
         return schedule.monitor.commit_scalars(
-            global_step=schedule.instance, chapter=self.chapter, **{
+            global_step=schedule.iteration, chapter=self.chapter, **{
                 name: sum(values)
                 for name, values in schedule.monitor.query(self.chapter, *self.names)
             })
@@ -30,23 +28,9 @@ class CommitScalarByMean(Extension):
 
     def __call__(self, schedule: 'Schedule') -> None:
         return schedule.monitor.commit_scalars(
-            global_step=schedule.instance, chapter=self.chapter, **{
+            global_step=schedule.iteration, chapter=self.chapter, **{
                 name: sum(values) / len(values)
                 for name, values in schedule.monitor.query(self.chapter, *self.names)
             })
 
 
-class CommitPRCurve(Extension):
-    def __init__(self, name: str, chapter: str):
-        super(CommitPRCurve, self).__init__()
-        self.name = name
-        self.chapter = chapter
-
-    def __call__(self, schedule: 'Schedule') -> None:
-        [(_, targets)] = schedule.monitor.query(self.chapter, f'{self.name}_targets')
-        [(_, predictions)] = schedule.monitor.query(self.chapter, f'{self.name}_predictions')
-        return schedule.monitor.commit_pr_curve(
-            name=self.name, global_step=schedule.instance, chapter=self.chapter,
-            targets=long_tensor(targets),
-            predictions=long_tensor(predictions),
-        )
