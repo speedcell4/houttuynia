e30091fc652c3c88da1356efe6785eea23500b17
speedcell4 <speedcell4@gmail.com>
2018-07-19 03:01:18 +0900

    fix(pooling): add pooling module in nn

--- houttuynia/nn/modules/__init__.py
+++ houttuynia/nn/modules/__init__.py
@@ -6,3 +6,4 @@ from houttuynia.nn.modules.metrics import *
 from houttuynia.nn.modules.architectures import *
 from houttuynia.nn.modules.utils import *
 from houttuynia.nn.modules.connections import *
+from houttuynia.nn.modules.pooling import *
