2cb92b3d51ae9fb9820bd99db97b0cbf117ea846
speedcell4 <speedcell4@gmail.com>
2018-07-23 15:03:03 +0900

    fix(extension): fix typo in warning_unused

--- houttuynia/schedules/extensions/warning_unused.py
+++ houttuynia/schedules/extensions/warning_unused.py
@@ -10,4 +10,4 @@ __all__ = [
 class WarningUnused(Extension):
     def __call__(self, schedule: 'Schedule') -> None:
         if schedule.monitor.memory.__len__() != 0:
-            warnings.warn(f'there are unused information in Monitor memory => {schedules.monitor.memory}')
+            warnings.warn(f'there are unused information in Monitor memory => {schedule.monitor.memory}')
